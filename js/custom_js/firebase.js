562296895cb54a94ade71e21da02c7922607398b
Rishi Soni <rishisoni6071@gmail.com>
2021-03-31 23:54:58 +0530

    Firebase Hosting

--- js/custom_js/firebase.js
+++ js/custom_js/firebase.js
@@ -1,12 +1,12 @@
 (() => {
-    // Your web app's Firebase configuration
+  // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyD7ZBWfaolICWPAvUUkm76my95TMufYmwQ",
-    authDomain: "rishi-soni-portfolio.firebaseapp.com",
-    projectId: "rishi-soni-portfolio",
-    storageBucket: "rishi-soni-portfolio.appspot.com",
-    messagingSenderId: "476778144972",
-    appId: "1:476778144972:web:b6f9bd281722803b234e69"
+    apiKey: "AIzaSyCZbf3OxcM_BBJQiuxMIfqPWWGPHV0gy-w",
+    authDomain: "rishisoni-portfolio.firebaseapp.com",
+    projectId: "rishisoni-portfolio",
+    storageBucket: "rishisoni-portfolio.appspot.com",
+    messagingSenderId: "383790404423",
+    appId: "1:383790404423:web:0c65445d8d803da020f262"
   };
   
   // Initialize Firebase
