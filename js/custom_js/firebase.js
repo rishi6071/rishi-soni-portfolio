ca227ae9e02503b0f8e38f055f46306ab86600ef
Rishi Soni <rishisoni6071@gmail.com>
2021-03-31 23:33:11 +0530

    Portfolio Initialised

---
+++ js/custom_js/firebase.js
@@ -0,0 +1,14 @@
+(() => {
+    // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD7ZBWfaolICWPAvUUkm76my95TMufYmwQ",
+    authDomain: "rishi-soni-portfolio.firebaseapp.com",
+    projectId: "rishi-soni-portfolio",
+    storageBucket: "rishi-soni-portfolio.appspot.com",
+    messagingSenderId: "476778144972",
+    appId: "1:476778144972:web:b6f9bd281722803b234e69"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+})()
