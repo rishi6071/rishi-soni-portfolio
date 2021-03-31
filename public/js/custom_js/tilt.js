3818bf2e68cb90998a31a58d0c84e63237bf2887
Rishi Soni <rishisoni6071@gmail.com>
2021-04-01 01:05:37 +0530

    Added tilt Effect

---
+++ public/js/custom_js/tilt.js
@@ -0,0 +1,11 @@
+(() => {
+    VanillaTilt.init(document.querySelector(".tilt-element"), {
+        max: 5,
+        speed: 200,
+        easing: "cubic-bezier(.03,.98,.52,.99)",
+        transition: true
+    });
+
+    //It also supports NodeList
+    VanillaTilt.init(document.querySelectorAll(".tilt-element"));
+})()
