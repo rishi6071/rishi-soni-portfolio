82c255f8e2917aa5790fa2250f03a1882c364d9b
Rishi Soni <rishisoni6071@gmail.com>
2021-04-02 00:59:34 +0530

    Added AOS and add Interactivity

--- public/js/custom_js/navbarToggler.js
+++ public/js/custom_js/navbarToggler.js
@@ -1,9 +1,22 @@
-const openMenu = function(element)   {
+// Animate on Scroll Library Initialisation
+(() => {
+    AOS.init({
+        startEvent: 'DOMContentLoaded',
+        once: true,
+        delay: 100,
+        easing: "ease",
+        duration: 1000
+    });
+})()
+
+// Open Humberger Menu
+const openMenu = function (element) {
     document.getElementById('bar').style.display = "none";
     document.getElementById('cross').style.display = "block";
 }
 
-const closeMenu = function(element)  {
+// Close Humberger Menu
+const closeMenu = function (element) {
     document.getElementById('bar').style.display = "block";
     document.getElementById('cross').style.display = "none";
 }
