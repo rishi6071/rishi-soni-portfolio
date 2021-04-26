7722b5928b4ba82b1bbcdbccf82060791d613ea2
Rishi Soni <rishisoni6071@gmail.com>
2021-04-26 16:58:47 +0530

    Bug Fixing in tilt.js

--- js/custom_js/tilt.js
+++ js/custom_js/tilt.js
@@ -1,15 +1,18 @@
 // Tilt.js Library Initialization
 (() => {
     try {
-        VanillaTilt.init(document.querySelector(".tilt-element"), {
-            max: 5,
-            speed: 200,
-            easing: "cubic-bezier(.03,.98,.52,.99)",
-            transition: true
-        });
-    
-        //It also supports NodeList
-        VanillaTilt.init(document.querySelectorAll(".tilt-element"));
+        if(window.innerWidth > 800) {
+            VanillaTilt.init(document.querySelector(".tilt-element"), {
+                max: 5,
+                axis: 'x',
+                speed: 200,
+                easing: "cubic-bezier(.03,.98,.52,.99)",
+                transition: true
+            });
+        
+            //It also supports NodeList
+            VanillaTilt.init(document.querySelectorAll(".tilt-element"));
+        }
     }
     catch(error) {
     }
