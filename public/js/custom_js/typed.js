3694e8bc089efd3967bd4e24ac070447456e3201
Rishi Soni <rishisoni6071@gmail.com>
2021-11-05 22:41:29 +0530

    Debug Build Error

--- public/js/custom_js/typed.js
+++ public/js/custom_js/typed.js
@@ -2,7 +2,7 @@
 (() => {
     try {
         var typed = new Typed('#my_name_box', {
-            strings: [, `Hello World! This is <span>Rishi Soni</span>`, `I am a <span>Frontend Web Developer</span>`, `I am an <span>RPA Developer</span>`],
+            strings: [``, `Hello World! This is <span>Rishi Soni</span>`, `I am a <span>Frontend Web Developer</span>`, `I am an <span>RPA Developer</span>`],
             startDelay: 800,
             backSpeed: 20,
             backDelay: 1500,
